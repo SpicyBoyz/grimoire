ccede152d0d8755a2e4509c3e6852f57acda3697
Luke Patton <lpatton@dosomething.org>
2018-10-11 10:33:23 -0400

    Removes lots of colors from the lists
    Also update the style of the individual player cards.

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,38 +6,38 @@ import 'react-drag-list/assets/index.css';
 import './List.css';
 
 class List extends React.Component {
-    _handleUpdate = (evt, updated) => {
-        console.log(evt); // tslint:disable-line
-        console.log(updated); // tslint:disable-line
-        // this.setState({
-        //   dataSource: [...updated, {
-        //     color: '#FFAA00',
-        //     title: 'Added Engineer',
-        //     text: 'Added Engineer',
-        //   }]
-        // })
-    };
+  _handleUpdate = (evt, updated) => {
+    console.log(evt); // tslint:disable-line
+    console.log(updated); // tslint:disable-line
+    // this.setState({
+    //   dataSource: [...updated, {
+    //     color: '#FFAA00',
+    //     title: 'Added Engineer',
+    //     text: 'Added Engineer',
+    //   }]
+    // })
+  };
 
-    render() {
-        return (
-            <div className="simple">
-                <ReactDragList
-                    dataSource={this.props.players}
-                    rowKey="title"
-                    row={(record, index) => (
-                        <div key={index} style={{ color: record.color }}>
-                            <div className="player-name">{record.title}</div>
-                            <span>{record.text}</span>
-                        </div>
-                    )}
-                    handles={false}
-                    className="player-list"
-                    rowClassName="player-item"
-                    onUpdate={this._handleUpdate}
-                />
+  render() {
+    return (
+      <div className="simple">
+        <ReactDragList
+          dataSource={this.props.players}
+          rowKey="title"
+          row={(record, index) => (
+            <div key={index}>
+              <div className="player-name">{record.title}</div>
+              <span className="player-title">{record.text}</span>
             </div>
-        );
-    }
+          )}
+          handles={false}
+          className="player-list"
+          rowClassName="player-item"
+          onUpdate={this._handleUpdate}
+        />
+      </div>
+    );
+  }
 }
 
 export default List;
