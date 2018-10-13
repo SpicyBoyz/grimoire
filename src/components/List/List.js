680b4a3cb941fe22ae243b5715aa90ed80186cbf
Luke <lkpttn@gmail.com>
2018-10-13 13:34:13 -0400

    Begin to pass initative up to the parent
    Still need to figure out how to target the right player to update their initiative

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,42 +3,49 @@ import React from 'react';
 import './List.css';
 
 class List extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.sortPlayers = this.sortPlayers.bind(this);
-  }
-  sortPlayers() {
-    // sort by value
-    let initiativeOrderArray = this.props.players.sort(function(a, b) {
-      return a.initiative - b.initiative;
-    });
+        this.sortPlayers = this.sortPlayers.bind(this);
+        this.handleInitiativeChange = this.handleInitiativeChange.bind(this);
+    }
 
-    this.setState({
-      deaconsDecoys: initiativeOrderArray.reverse(),
-    });
-  }
+    handleInitiativeChange(event) {
+        // How can we pass in the proper player
+        this.props.handleInitiativeChange(event.target.value, 'player');
+        this.sortPlayers();
+    }
 
-  render() {
-    return (
-      <div>
-        {this.props.players.map(function(record, index) {
-          return (
-            <div className="player-item" key={index}>
-              <div className="player-name">{record.title}</div>
-              <span className="player-title">{record.text}</span>
-              <input
-                className="player-initiative"
-                type="text"
-                placeholder={record.initiative}
-              />
+    sortPlayers() {
+        // sort by value
+        let initiativeOrderArray = this.props.players.sort(function(a, b) {
+            return a.initiative - b.initiative;
+        });
+
+        this.setState({
+            deaconsDecoys: initiativeOrderArray.reverse(),
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.players.map((record, index) => (
+                    <div className="player-item" key={index}>
+                        <div className="player-name">{record.title}</div>
+                        <span className="player-title">{record.text}</span>
+                        <input
+                            className="player-initiative"
+                            type="text"
+                            placeholder={record.initiative}
+                            onBlur={this.handleInitiativeChange}
+                        />
+                    </div>
+                ))}
+                <button onClick={this.sortPlayers}>Sort</button>
             </div>
-          );
-        })}
-        <button onClick={this.sortPlayers}>Sort</button>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default List;
