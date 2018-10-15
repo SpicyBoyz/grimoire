c3615536bbb0ef1fdff0fe693a0b9c7068d2a3bf
Luke Patton <lpatton@dosomething.org>
2018-10-15 16:09:43 -0400

    Swaps out live sort for true sort button functionality
    Also adds dexterity values to characters for placeholder text.

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,57 +3,53 @@ import React from 'react';
 import './List.css';
 
 class List extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.sortPlayers = this.sortPlayers.bind(this);
-        this.handleInitiativeChange = this.handleInitiativeChange.bind(this);
-    }
+    this.sortPlayers = this.sortPlayers.bind(this);
+    this.handleInitiativeChange = this.handleInitiativeChange.bind(this);
+  }
 
-    handleInitiativeChange(event, player) {
-        let initiative = event.target.value;
-        if (initiative === '') {
-            return;
-        }
+  handleInitiativeChange(event, player) {
+    let initiative = event.target.value;
 
-        this.props.handleInitiativeChange(initiative, player);
-        this.sortPlayers();
-        // Find a way to reset the form values
-    }
+    this.props.handleInitiativeChange(initiative, player);
+    event.preventDefault();
+  }
 
-    sortPlayers() {
-        // sort by value
-        let initiativeOrderArray = this.props.players.sort(function(a, b) {
-            return a.initiative - b.initiative;
-        });
+  sortPlayers() {
+    // sort by value
+    let initiativeOrderArray = this.props.players.sort(function(a, b) {
+      return a.initiative - b.initiative;
+    });
 
-        this.setState({
-            deaconsDecoys: initiativeOrderArray.reverse(),
-        });
-    }
+    this.setState({
+      deaconsDecoys: initiativeOrderArray.reverse(),
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                {this.props.players.map((player, index) => (
-                    <div className="player-item" key={index}>
-                        <div className="player-name">{player.title}</div>
-                        <span className="player-title">{player.text}</span>
-                        <input
-                            className="player-initiative"
-                            type="text"
-                            placeholder={player.initiative}
-                            defaultValue=""
-                            onBlur={event =>
-                                this.handleInitiativeChange(event, player)
-                            }
-                        />
-                    </div>
-                ))}
-                <button onClick={this.sortPlayers}>Sort</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        {this.props.players.map((player, index) => (
+          <div className="player-item" key={index}>
+            <div className="player-name">{player.title}</div>
+            <span className="player-title">{player.text}</span>
+            <form id="initiative">
+              <input
+                className="player-initiative"
+                type="text"
+                placeholder={player.dexterity}
+                value={player.initiative}
+                onChange={event => this.handleInitiativeChange(event, player)}
+              />
+            </form>
+          </div>
+        ))}
+        <button onClick={this.sortPlayers}>Sort</button>
+      </div>
+    );
+  }
 }
 
 export default List;
