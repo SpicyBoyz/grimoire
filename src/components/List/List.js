d0bc6d46bc893277e445f24283c4495c84cbc368
Luke <lkpttn@gmail.com>
2018-10-20 15:26:16 -0400

    Basic style to enhance current player

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,46 +3,64 @@ import React from 'react';
 import './List.css';
 
 class List extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleInitiativeChange = this.handleInitiativeChange.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.handleInitiativeChange = this.handleInitiativeChange.bind(this);
+    }
 
-  handleInitiativeChange(event, player) {
-    let initiative = event.target.value;
+    handleInitiativeChange(event, player) {
+        let initiative = event.target.value;
 
-    this.props.handleInitiativeChange(initiative, player);
-    event.preventDefault();
-  }
+        this.props.handleInitiativeChange(initiative, player);
+        event.preventDefault();
+    }
 
-  render() {
-    return (
-      <div>
-        <ul className="player-list">
-          {this.props.players.map((player, index) => (
-            <li className="player-item" key={index}>
-              <div class="player-info">
-                <div className="player-name">{player.title}</div>
-                <span className="player-title">{player.text}</span>
-              </div>
-              <form id="initiative">
-                <input
-                  className="player-initiative"
-                  type="text"
-                  placeholder={player.dexterity}
-                  value={player.initiative}
-                  onChange={event => this.handleInitiativeChange(event, player)}
-                />
-              </form>
-            </li>
-          ))}
-        </ul>
-        <button className="action-button" onClick={this.props.handleSort}>
-          Sort
-        </button>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <ul className="player-list">
+                    {this.props.players.map((player, index) => (
+                        <li
+                            className={
+                                'player-item ' +
+                                (index === 0 ? 'current-turn' : '')
+                            }
+                            key={index}
+                        >
+                            <div class="player-info">
+                                <div className="player-name">
+                                    {player.title}
+                                </div>
+                                <span className="player-title">
+                                    {player.text}
+                                </span>
+                            </div>
+                            <form id="initiative">
+                                <input
+                                    className="player-initiative"
+                                    type="text"
+                                    placeholder={player.dexterity}
+                                    value={player.initiative}
+                                    onChange={event =>
+                                        this.handleInitiativeChange(
+                                            event,
+                                            player,
+                                        )
+                                    }
+                                />
+                            </form>
+                        </li>
+                    ))}
+                </ul>
+                <button
+                    className="action-button"
+                    onClick={this.props.handleSort}
+                >
+                    Sort
+                </button>
+            </div>
+        );
+    }
 }
 
 export default List;
